c332e164fea475e89d8c7356f24348a131d4f45b
seanmcquaid <sean.mcquaid2@verizon.net>
2018-11-15 21:35:59 -0500

    Finished paragraph problem

--- dictionaryExercises.py
+++ dictionaryExercises.py
@@ -87,6 +87,22 @@ print letter_histogram("shrimp")
 # {'to': 2, 'be': 2, 'or': 1, 'not': 1}
 
 
+# found this online
+def word_count(str):
+    counts = dict()
+    words = str.split()
+
+    for word in words:
+        if word in counts:
+            counts[word] += 1
+        else:
+            counts[word] = 1
+
+    return counts
+
+print( word_count('the quick brown fox jumps over the lazy dog.'))
+
+
 # In this exercise, are you using dynamic keys or fixed keys?
 
 # Bonus Challenge
